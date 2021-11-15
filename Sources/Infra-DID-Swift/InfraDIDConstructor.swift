b6df810270b71f953384b4ac816183b2f2bcb928
Lee-Chang-Hwa <step323@naver.com>
2021-11-15 17:02:44 +0900

    Addition - Key Pair Generate Funcc

--- Sources/Infra-DID-Swift/InfraDIDConstructor.swift
+++ Sources/Infra-DID-Swift/InfraDIDConstructor.swift
@@ -7,11 +7,47 @@
 
 import Foundation
 import EosioSwift
+import secp256k1
+import secp256k1_implementation
+
 
 public class InfraDIDConstructor {
   
+  private func generateRandomBytes(bytes: Int) -> Data? {
+
+      var keyData = Data(count: bytes)
+      let result = keyData.withUnsafeMutableBytes {
+          SecRandomCopyBytes(kSecRandomDefault, bytes, $0.baseAddress!)
+      }
+      if result == errSecSuccess {
+        return keyData
+      } else {
+          print("Problem generating random bytes")
+          return nil
+      }
+  }
+  
+  public func createPubKeyDID(networkID: String) -> [String: String] {
+    
+    guard let pvData: Data = generateRandomBytes(bytes: 32) else { return [:]}
+    let keyPair = try! secp256k1.Signing.PrivateKey.init(rawRepresentation: pvData)
+    
+    let privateKey: String = keyPair.rawRepresentation.toEosioK1PrivateKey
+    let publicKey: String = keyPair.publicKey.rawRepresentation.toEosioK1PublicKey
+    let did = "did:infra:\(networkID):\(publicKey)"
+    
+    
+    return ["did": did, "publicKey": publicKey, "privateKey": privateKey]
+  }
   
   public init() {
-    print(Data.init().toEosioK1PublicKey)
+
+    guard let pvData: Data = generateRandomBytes(bytes: 32) else { return }
+    
+    let privateKey = try! secp256k1.Signing.PrivateKey.init(rawRepresentation: pvData)
+    
+    print(privateKey.rawRepresentation.toEosioK1PrivateKey)
+    print(privateKey.publicKey.rawRepresentation.toEosioK1PublicKey)
+
   }
 }
--- Tests/Infra-DID-SwiftTests/Infra_DID_SwiftTests.swift
+++ Tests/Infra-DID-SwiftTests/Infra_DID_SwiftTests.swift
@@ -6,6 +6,7 @@ final class Infra_DID_SwiftTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+      print(InfraDIDConstructor())
         XCTAssertEqual(Infra_DID_Swift().text, "Hello, World!")
     }
 }
